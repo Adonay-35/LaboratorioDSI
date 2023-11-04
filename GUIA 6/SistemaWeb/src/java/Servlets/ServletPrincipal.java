b92f9db7df97b942b2356e00e8e702ffa003a95b
Adonay-35 <adonaysalazar5@gmail.com>
2023-11-04 09:37:20 -0600

    GUIA 6
    Guia 6

---
+++ GUIA 6/SistemaWeb/src/java/Servlets/ServletPrincipal.java
@@ -0,0 +1,177 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+//Agregar esto
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+// y esto
+import Models.ViewModelEmpleados;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class ServletPrincipal extends HttpServlet {
+
+    private final String usuario = "Login_Sysadmin_PedroHernandez";
+    private final String contrasenia = "root";
+    private final String servidor = "localhost:1433";
+    private final String bd = "Panaderia";
+    
+    String url = "jdbc:sqlserver://"
+            + servidor
+            + ";databaseName=" + bd
+            + ";user=" + usuario
+            + ";password=" + contrasenia
+            + ";encrypt=false;trustServerCertificate=false;";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletPrincipal</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletPrincipal at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    public void mostrarEmpleados(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            try(Connection conn = DriverManager.getConnection(url)){
+            //try (Connection conn = DriverManager.getConnection(url);) {
+                request.setAttribute("mensaje_conexion", "Ok!");
+                String sqlQuery = "select * from Empleados";
+                PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
+                ResultSet rs = pstmt.executeQuery();
+                ArrayList<ViewModelEmpleados> listaEmpleados = new ArrayList<>();
+                while (rs.next()) {
+                    ViewModelEmpleados empleado = new ViewModelEmpleados();
+                    empleado.setID_Empleado(rs.getInt("ID_Empleado"));
+                    empleado.setDui(rs.getString("dui"));
+                    empleado.setIsss(rs.getString("isss"));
+                    empleado.setNombresEmpleado(rs.getString("nombresEmpleado"));
+                    empleado.setApellidosEmpleado(rs.getString("apellidosEmpleado"));
+                    empleado.setFechaNacEmpleado(rs.getDate("fechaNacEmpleado"));
+                    empleado.setTelefono(rs.getString("telefono"));                   
+                    empleado.setID_Cargo(rs.getInt("ID_Cargo"));
+                    empleado.setID_Direccion(rs.getInt("ID_Direccion"));
+                    listaEmpleados.add(empleado);
+                }               
+                request.setAttribute("listaEmpleados", listaEmpleados);
+
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            request.setAttribute("mensaje_conexion", ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+        
+        if(accion == null){
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        } else if(accion.equals("Login")){
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        } else if(accion.equals("RegistroEmpleados")){
+            request.getRequestDispatcher("/RegistroEmpleados.html").forward(request, response);
+        } else if(accion.equals("RegistroClientes")){
+            request.getRequestDispatcher("/RegistroClientes.html").forward(request, response);
+        } else if(accion.equals("RegistroProductos")){
+            request.getRequestDispatcher("/RegistroProductos.html").forward(request, response);
+        } else if(accion.equals("GestionarEmpleados")){
+            mostrarEmpleados(request, response);
+            request.getRequestDispatcher("/GestionarEmpleados.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String accion = request.getParameter("accion");
+        
+        if(accion.equals("Login")){
+            String usuario = request.getParameter("tfUsuario");
+            String contrasenia = request.getParameter("tfContrasenia");
+            
+            try(PrintWriter print = response.getWriter()){
+                if(usuario.equals("admin") && contrasenia.equals("root")){
+                    request.getRequestDispatcher("/PanelAdministrador.jsp").forward(request, response);
+                }else{
+                    print.println("<!DOCTYPE html>");
+                    print.println("<html>");
+                    print.println("<head>");
+                    print.println("<title>Login Sistema Panaderia</title>");
+                    print.println("</head>");
+                    print.println("<body>");
+                    print.println("<h1>ERROR: El usuario o la contraseña son erróneos.</h1>");
+                    print.println("</body>");
+                    print.println("</html>");
+                    
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
